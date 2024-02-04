696229c35e32810db7c06dda580848c1743ab929
James Wilcock <jameswilcock8@gmail.com>
2024-02-03 16:47:12 -0800

    Added rle bw encode and decode

--- src/lib.rs
+++ src/lib.rs
@@ -25,10 +25,59 @@ fn decode_str(input: &str) -> String {
     unimplemented!()
 }
 
+#[pyfunction]
+fn encode_bw_image(image: Vec<Vec<u8>>) -> Vec<Vec<(u8, u8)>> {
+    let mut encoded_image = Vec::new();
+
+    for row in image {
+        let mut encoded_row = Vec::new();
+        let mut current_pixel = row[0];
+        let mut count = 0u8;
+
+        for &pixel in row.iter() {
+            if pixel == current_pixel && count < u8::MAX {
+                // Increment count if the current pixel matches the previous, and we haven't hit the count limit
+                count += 1;
+            } else {
+                // Otherwise, push the pixel and its count to the encoded row, and reset the count
+                encoded_row.push((current_pixel, count));
+                current_pixel = pixel;
+                count = 1;
+            }
+        }
+        // Don't forget to add the last sequence
+        encoded_row.push((current_pixel, count));
+
+        encoded_image.push(encoded_row);
+    }
+
+    encoded_image
+}
+
+#[derive(FromPyObject)]
+struct RustyTuple(u8, u8);
+
+#[pyfunction]
+fn decode_bw_image(encoded_image: Vec<Vec<RustyTuple>>) -> Vec<Vec<u8>> {
+    let mut decoded_image = Vec::new();
+    for row in encoded_image {
+        let mut encoded_row: Vec<u8> = Vec::new();
+        for rle_item in row {
+            for _ in 0..rle_item.1 {
+                encoded_row.push(rle_item.0);
+            }
+        }
+        decoded_image.push(encoded_row);
+    }
+    decoded_image
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn rustle(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(encode_str, m)?)?;
     m.add_function(wrap_pyfunction!(decode_str, m)?)?;
+    m.add_function(wrap_pyfunction!(encode_bw_image, m)?)?;
+    m.add_function(wrap_pyfunction!(decode_bw_image, m)?)?;
     Ok(())
 }
